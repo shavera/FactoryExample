87dee7aa353356dfb4cf4728f2e54236d57957ad
Alex Shaver <alexshaver@gmail.com>
2017-08-30 19:25:40 -0400

    Create alias for a factory for Alpha classes

--- Alpha.h
+++ Alpha.h
@@ -5,9 +5,13 @@
 #ifndef FACTORYEXAMPLE_ALPHA_H
 #define FACTORYEXAMPLE_ALPHA_H
 
+#include "GenericFactory.h"
+
 class Alpha {
 public:
     virtual void print() = 0;
 };
+
+using AlphaFactory = GenericFactory<Alpha>;
 
 #endif //FACTORYEXAMPLE_ALPHA_H
