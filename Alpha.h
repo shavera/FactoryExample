4d0ef943bb0cd14eabeffe67926fe50edc97abe3
Alex Shaver <alexshaver@gmail.com>
2017-08-30 18:45:27 -0400

    Add simple interface class

---
+++ Alpha.h
@@ -0,0 +1,13 @@
+//
+// Created by Alex Shaver on 8/30/17.
+//
+
+#ifndef FACTORYEXAMPLE_ALPHA_H
+#define FACTORYEXAMPLE_ALPHA_H
+
+class Alpha {
+public:
+    virtual void print() = 0;
+};
+
+#endif //FACTORYEXAMPLE_ALPHA_H
