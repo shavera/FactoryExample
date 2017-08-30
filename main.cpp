391ec7a99526ec5c4b711af94585d6cf462eae8c
Alex Shaver <alexshaver@gmail.com>
2017-08-30 19:17:22 -0400

    Use implementations to demonstrate behaviour
    Each implementation is being contained by a unique_ptr to the base class, and the function call is made of the base class.

--- main.cpp
+++ main.cpp
@@ -1,6 +1,22 @@
 #include <iostream>
+#include <memory>
+
+#include "Anchor.h"
+#include "Apple.h"
+#include "Axle.h"
 
 int main() {
+    std::unique_ptr<Alpha> a{nullptr};
+
+    a = std::make_unique<Anchor>();
+    a->print();
+
+    a = std::make_unique<Apple>();
+    a->print();
+
+    a = std::make_unique<Axle>();
+    a->print();
+
     std::cout << "Hello, World!" << std::endl;
     return 0;
 }
