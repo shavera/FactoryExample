e55b1f4429dd392b5af6b41c4b390676628b1043
Alex Shaver <alexshaver@gmail.com>
2017-08-30 21:19:56 -0400

    Add Initializer Alias

---
+++ Initializer.h
@@ -0,0 +1,13 @@
+//
+// Created by Alex Shaver on 8/30/17.
+//
+
+#ifndef FACTORYEXAMPLE_INITIALIZER_H
+#define FACTORYEXAMPLE_INITIALIZER_H
+
+#include <map>
+#include <string>
+
+using Initializer = std::map<std::string, std::string>;
+
+#endif //FACTORYEXAMPLE_INITIALIZER_H
